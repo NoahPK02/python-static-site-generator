34b6dc56829210a8a1a8b074ffbbe35bf487f8a3
NoahPK02 <noahpkmail@gmail.com>
2022-07-20 12:16:45 +0000

    Debugging

--- ssg.py
+++ ssg.py
@@ -3,7 +3,7 @@ from ssg.site import Site
 import ssg.parsers
 
 def main(source="content", dest="dist"):
-	config = {"source":source, "dest":dest, "parsers":[ssg.parsers.ResourceParser(), ssg.parsers.MArkdownParser(), ssg.parsers.ReStructuredTExtParser()]}
+	config = {"source":source, "dest":dest, "parsers":[ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]}
 	theSite = Site(**config).build()
 
 typer.run(main)
